179f18cbc7feabd2aadfc0abb4cc630c1ce9b23f
Peter Nguyen <nguye5he@mail.uc.edu>
2026-04-15 00:25:53 -0400

    added platforms and more scripting (sprite flip, collider, physics)

---
+++ Assets/_Main_/Script/AnimationStrings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal class AnimationStrings
+{
+    public static string IsMoving = "IsMoving";
+    public static string IsRunning = "IsRunning";
+}
--- Assets/_Main_/Script/Player Character/PlayerController.cs
+++ Assets/_Main_/Script/Player Character/PlayerController.cs
@@ -6,27 +6,61 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float walkSpeed = 5f;
+    public float runSpeed = 8f;
     Vector2 moveInput;
+
+    public float CurrentSpeed
+    {
+        get
+        {
+            if (IsMoving && IsRunning)
+            {
+                return runSpeed;
+            }
+            else if (IsMoving)
+            {
+                return walkSpeed;
+            }
+            else
+            {
+                return 0f;
+            }
+        }
+    }
 
     [SerializeField]
     private Boolean _isMoving = false;
     public bool IsMoving { get { return _isMoving; } 
         private set { _isMoving = value; 
-            animator.SetBool("IsMoving", _isMoving); } }
+            animator.SetBool(AnimationStrings.IsMoving, _isMoving); } }
 
     [SerializeField]
-    private bool isRunning = false;
-    public bool IsRunning { get { return isRunning; } 
-        private set { isRunning = value; 
-            animator.SetBool("IsRunning", isRunning); }
+    private bool _isRunning = false;
+    public bool IsRunning { get { return _isRunning; } 
+        private set { _isRunning = value; 
+            animator.SetBool(AnimationStrings.IsRunning, _isRunning); }
     }
 
-
     Animator animator;
+
+    private bool _isFacingRight = true;
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale = new Vector2(transform.localScale.x * -1, 1);
+            }
+            _isFacingRight = value;
+        }
+    }
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -43,7 +77,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput.x * CurrentSpeed, rb.linearVelocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context){
@@ -51,6 +85,20 @@ public class PlayerController : MonoBehaviour
 
         IsMoving = moveInput != Vector2.zero;
 
+        SetFacingDir(moveInput);
+
+    }
+
+    private void SetFacingDir(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
     }
 
     public void OnRun(InputAction.CallbackContext context){
